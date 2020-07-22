a1f4a1e07a862572e5ccdd26e21fb82a7f3b65de
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-07-22 18:08:28 +0600

    final

---
+++ js/todos-app.js
@@ -0,0 +1,34 @@
+
+'use strict'
+
+let todos= getSavedTodos()
+let filters={
+    searchText :'',
+    hideCompleted: false
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-todos').addEventListener('input', function (e) {
+    filters.searchText = e.target.value.toLowerCase()
+    renderTodos(todos, filters)
+})
+
+document.getElementById('add-todos-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    if(e.target.elements.newTodo.value.trim()!==''){
+     todos.push({
+         id: uuidv4(),
+         text: e.target.elements.newTodo.value.trim(),
+         completed: false
+     })
+     saveTodos(todos)
+     renderTodos(todos, filters)
+     e.target.elements.newTodo.value = ''
+    } 
+})
+
+document.querySelector('#hideCheckbox').addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
